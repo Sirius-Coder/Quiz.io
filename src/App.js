2e9bb7ed604a2d654032f1ac427747005afdef84
Sirius-Coder <eche17056@rgipt.ac.in>
2020-06-19 19:16:25 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import{Route,Switch} from 'react-router-dom';
+import Startup from './components/startup.jsx';
+import EnterName from './components/enterQuizName.jsx';
+import MakeQuiz from './components/makeQuiz.jsx';
+import SubmitQuiz from './components/submitQuiz.jsx';
+import ViewQuiz from './components/viewQuiz.jsx';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    
+    <div className="main-container">
+     
+      <Switch>
+        
+        <Route path="/" component={Startup} exact />
+        <Route path="/enterName" component={EnterName} />
+        <Route path="/makeQuiz" component={MakeQuiz} />
+        <Route path='/submitQuiz' component={SubmitQuiz}/>
+        <Route path='/viewQuiz' component={ViewQuiz}/>
+        
+      
+      </Switch>
+      
     </div>
-  );
+    
+  )
 }
 
 export default App;
